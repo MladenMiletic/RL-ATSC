38b9f88c5df00b38273d71548552507062bbd341
MladenMiletic <mladenmileticlol@gmail.com>
2024-01-17 19:07:11 +0100

    started working on GasNetwork

---
+++ Quest.ML/Clustering/Neural/GasNetwork.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quest.ML.Clustering.Neural
+{
+    public class GasNetwork
+    {
+		private int id;
+
+		public int ID
+		{
+			get
+			{
+				return id;
+			}
+			private set
+			{
+				id = value;
+			}
+		}
+
+		private List<GasNeuron> neurons;
+
+		public List<GasNeuron> Neurons
+		{
+			get
+			{
+				return neurons;
+			}
+			private set
+			{
+				neurons = value;
+			}
+		}
+
+
+	}
+}
