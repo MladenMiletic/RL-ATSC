f6620465f4d1c2a6eab84b7099bd587b9e7cdcf3
Mladen Miletić <mmiletic@FPZ.HR>
2024-01-17 14:30:35 +0100

    compute for neuron complete

--- Quest.ML/Clustering/Neural/Neuron.cs
+++ Quest.ML/Clustering/Neural/Neuron.cs
@@ -8,6 +8,77 @@ namespace Quest.ML.Clustering.Neural
 {
     public class Neuron
     {
+		private int id;
+		private double[] weights;
+        private int age;
+		private int activationCounter;
+		private double output;
 
-    }
+        public int ID
+		{
+			get
+			{
+				return id;
+			}
+			set
+			{
+				id = value;
+			}
+		}
+		public double[] Weights
+		{
+			get
+			{
+				return weights;
+			}
+			private set
+			{
+				weights = value;
+			}
+		}
+		public int Age
+		{
+			get
+			{
+				return age;
+			}
+			set
+			{
+				age = value;
+			}
+		}
+		public double Output
+		{
+            get
+            {
+                return output;
+            }
+            private set
+            {
+                output = value;
+            }
+        }
+
+		public Neuron(int id, int numInputDimensions)
+		{
+			this.id = id;
+			weights = new double[numInputDimensions];
+			age = 0;
+			activationCounter = 0;
+		}
+
+		public double Compute(double[] inputs)
+		{
+			double sumOfSquares = 0;
+
+			for (int i = 0; i < weights.Length; i++)
+			{
+                sumOfSquares += Math.Pow(weights[i] - inputs[i], 2);
+            }
+
+			return Math.Sqrt(sumOfSquares);
+		}
+
+
+	}
 }
