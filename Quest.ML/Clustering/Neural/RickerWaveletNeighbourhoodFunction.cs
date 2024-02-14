a165df28a40952246023098051c7966905ce7ec6
Mladen Miletić <mmiletic@FPZ.HR>
2024-02-14 13:15:02 +0100

    Added Ricker wavelet neighbourhood function

---
+++ Quest.ML/Clustering/Neural/RickerWaveletNeighbourhoodFunction.cs
@@ -0,0 +1,38 @@
+﻿using Quest.ML.Clustering.Neural.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quest.ML.Clustering.Neural
+{
+    /// <summary>
+    /// Ricker wavelet is sometimes called "Mexican hat function" which is culturaly insensitive,
+    /// hence the proper name is used here
+    /// </summary>
+    public class RickerWaveletNeighbourhoodFunction : INeighbourhoodFunction
+    {
+        private double neighbourhoodWidth = 5;
+        public RickerWaveletNeighbourhoodFunction()
+        {
+
+        }
+        public RickerWaveletNeighbourhoodFunction(double neighbourhoodWidth)
+        {
+            NeighbourhoodWidth = neighbourhoodWidth;
+        }
+
+        public double NeighbourhoodWidth
+        {
+            get => neighbourhoodWidth;
+            set => neighbourhoodWidth = value;
+        }
+
+        public double Calculate(double distance)
+        {
+            double distanceSquared = distance * distance;
+            return (1- distanceSquared/NeighbourhoodWidth) *(Math.Exp(-distanceSquared / (2 * neighbourhoodWidth)));
+        }
+    }
+}
