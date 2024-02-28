3cf3a3eb775bef525e2c0ac7eb519f70802ddc20
Mladen Miletić <mmiletic@FPZ.HR>
2024-02-28 16:47:36 +0100

    preparing the simulation loop

--- ATSC/Program.cs
+++ ATSC/Program.cs
@@ -9,7 +9,15 @@ namespace ATSC
 
     public class Program
     {
-        public static GasQAgent Agent;
+        private static GasQAgent? Agent;
+        private static VissimEnvironment? Environment;
+        private static List<double[]> StateList = [];
+        private static readonly Random rng = new();
+
+        private static readonly int numberOfSimulations = 500;
+        private static readonly int initializationTime = 900;
+        private static readonly int simulationDuration = 59400;
+        private static readonly int agentTimeStep = 300;
         
         public static void Main(string[] args)
         {
@@ -19,12 +27,76 @@ namespace ATSC
             GasQAgentParameters gasQAgentParameters = new GasQAgentParameters(4, gasNetwork, selectionPolicy);
 
             Agent = new GasQAgent(gasQAgentParameters);
+            Environment = new VissimEnvironment();
 
-            VissimEnvironment environment = new VissimEnvironment();
+            for (int i = 0; i <= numberOfSimulations; i++)
+            {
+                PerformLearningSimulation();
+                var RandomStateListPermutation = StateList.OrderBy(_ => rng.Next()).ToList();
+                var gasNetworkError = RunGasNetworkLearningEpoch(RandomStateListPermutation, Agent.gasNetworkStateIdentifier);
+                //DO SOMETHING WITH NETWORK PARAMETERS (Neighbourhood function, learning rate)
 
+                //DO SOMETHING WITH RESULTS
+            }
+        }
 
+        private static void PerformLearningSimulation()
+        {
+            
+            if (Agent == null)
+            {
+                throw new NullReferenceException("Agent can not be null during learning simulation!");
+            }
+            if (Environment == null)
+            {
+                throw new NullReferenceException("Environment can not be null during learning simulation!");
+            }
 
-
+            StateList = [];
+            bool firstAction = true;
+            double delayBeforeAction = 0;
+            int previousStateId = 0;
+            Environment.InitializeSimulation(initializationTime);
+            for (int j = 0; j < simulationDuration - initializationTime; j++)
+            {
+                Environment.RunSimulationStep();
+                if (j == simulationDuration - initializationTime - 1)
+                {
+                    break;
+                }
+                if (j % agentTimeStep != 0)
+                {
+                    continue;
+                }
+                StateList.Add(Environment.GetLaneStateInfo());
+                int stateId = Agent.GetStateId(StateList.Last());
+                int actionId = Agent.SelectAction(stateId);
+                Environment.PerformAction(actionId);
+                if (!firstAction)
+                {
+                    double delayAfterAction = Environment.GetAverageDelayOfLastTimeStep();
+                    double reward = delayBeforeAction - delayAfterAction;
+                    delayBeforeAction = delayAfterAction;
+                    Agent.Learn(previousStateId, actionId, stateId, reward);
+                    previousStateId = stateId;
+                }
+                else
+                {
+                    delayBeforeAction = Environment.GetAverageDelayOfLastTimeStep();
+                    previousStateId = stateId;
+                    firstAction = false;
+                }
+            }
+        }
+        private static double RunGasNetworkLearningEpoch(List<double[]> dataList, GasNetwork network)
+        {
+            double error = 0;
+            foreach (double[] data in dataList)
+            {
+                error += network.Learn(data);
+            }
+            network.IncrementAgeEdges();
+            return error / dataList.Count;
         }
     }
 }
--- VissimEnvironment/VissimEnvironment.cs
+++ VissimEnvironment/VissimEnvironment.cs
@@ -1,4 +1,6 @@
-﻿namespace VissimEnv
+﻿using VISSIMLIB;
+
+namespace VissimEnv
 {
     /// <summary>
     /// The purpose of this class is to wrap Vissim simulator in a simple way to make it easier to
@@ -6,7 +8,12 @@
     /// </summary>
     public class VissimEnvironment
     {
-        private IVissim simulator;
+        private readonly IVissim simulator;
+        private List<ILane> lanes = [];
+
+        private double[] cavsScalingFactor;
+        private double[] maxCavsPerLane;
+        private readonly object[] Attributes_veh = ["No", "VehType"];
 
         public VissimEnvironment()
         {
@@ -16,12 +23,78 @@
         public void LoadNetwork(string path)
         {
             simulator.LoadNet(path);
+            //get lanes
+            foreach (ILink Link in simulator.Net.Links)
+            {
+                if (!Convert.ToBoolean(Link.get_AttValue("IsConn")))
+                {
+                    foreach (ILane Lane in Link.Lanes)
+                    {
+                        lanes.Add(Lane);
+                    }
+                }
+
+            }
+            cavsScalingFactor = new double[lanes.Count];
+            maxCavsPerLane = new double[lanes.Count];
         }
 
         public void RunSimulationStep()
         {
             simulator.Simulation.RunSingleStep();
         }
-        
+
+        public double[] GetLaneStateInfo()
+        {
+            double[] cavsPerLane = new double[lanes.Count];
+            Parallel.For(0, lanes.Count, i =>
+            {
+                cavsPerLane[i] = 0;
+                object[,] all_veh_attributes = (object[,])lanes[i].Vehs.GetMultipleAttributes(Attributes_veh);
+                for (int j = 0; j < all_veh_attributes.GetLength(0); j++)
+                {
+                    if (Convert.ToDouble(all_veh_attributes[j, 1]) == 640)
+                    {
+                        cavsPerLane[i]++;
+                    }
+                }
+                if (cavsPerLane[i] > maxCavsPerLane[i])
+                {
+                    maxCavsPerLane[i] = cavsPerLane[i];
+                    cavsScalingFactor[i] = 1.0 / cavsPerLane[i];
+                }
+                cavsPerLane[i] = cavsPerLane[i] * cavsScalingFactor[i];
+            });
+            return cavsPerLane;
+        }
+        public double GetTotalTravelTime()
+        {
+            return (double)simulator.Net.VehicleNetworkPerformanceMeasurement.get_AttValue("TravTmTot(Current, Total, All)");
+        }
+        public double GetAverageDelay()
+        {
+            return (double)simulator.Net.VehicleNetworkPerformanceMeasurement.get_AttValue("DelayAvg(Current, Average, All)");
+        }
+        public int GetTotalNumberOfStops()
+        {
+            return (int)simulator.Net.VehicleNetworkPerformanceMeasurement.get_AttValue("StopsTot(Current, Total, All");
+        }
+        public double GetAverageDelayOfLastTimeStep()
+        {
+            return (double)simulator.Net.VehicleNetworkPerformanceMeasurement.get_AttValue("DelayAvg(Current, Last, All)");
+        }
+
+        public void InitializeSimulation(int initTime)
+        {
+            for (int i = 0; i < initTime; i++)
+            {
+                RunSimulationStep();
+            }
+        }
+
+        public void PerformAction(int actionId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
